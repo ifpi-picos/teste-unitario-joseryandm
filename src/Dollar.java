3fbb3342114713f8f09e9295080287c4819f796b
GilbertoJr22 <capic.2020118tads0134@aluno.ifpi.edu.br>
2022-04-19 12:51:50 -0300

    implementado a classe dollar

--- src/Dollar.java
+++ src/Dollar.java
@@ -1,9 +1,9 @@
-public class Dollar {
+class Dollar {
   int amount;
   Dollar(int amount) {
     this.amount= amount;
   }
-  void times(int multiplier) {
-    amount= amount * multiplier;
+  Dollar times(int multiplier) {
+    return new Dollar(amount * multiplier);
   }
 }
--- src/DollarTeste.java
+++ src/DollarTeste.java
@@ -2,12 +2,10 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-public class DollarTeste{
-
-    @Test
-    public void testMultiplication() {
-        Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
-     }
-}
+public class DollarTest {
+  @Test
+  public void testMultiplication(){
+    Dollar five = new Dollar(5);
+    assertEquals(new Dollar(10), five.times(2));
+    assertEquals(new Dollar(15), five.times(3));
+  }
