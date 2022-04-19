91f4f49123ce2c3f78afff350f0609e9069a7a66
joseryandm <joae.ryan.123@gmail.com>
2022-04-19 13:08:37 -0300

    adicionando a função equals

--- src/Dollar.java
+++ src/Dollar.java
@@ -1,9 +1,13 @@
-class Dollar {
-  int amount;
+public class Dollar {
+  private int amount;
   Dollar(int amount) {
     this.amount= amount;
   }
   Dollar times(int multiplier) {
     return new Dollar(amount * multiplier);
+  }
+  public boolean equals(Object object)  {
+    Dollar dollar = (Dollar) object;
+    return amount == dollar.amount;
   }
 }
--- src/DollarTeste.java
+++ src/DollarTeste.java
@@ -1,4 +1,4 @@
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
@@ -8,4 +8,10 @@ public class DollarTest {
     Dollar five = new Dollar(5);
     assertEquals(new Dollar(10), five.times(2));
     assertEquals(new Dollar(15), five.times(3));
+  }
+
+  @Test
+  public void testEquality() {
+    assertTrue(new Dollar(5).equals(new Dollar(5)));
+    assertFalse(new Dollar(5).equals(new Dollar(6)));
   }
