711659772d1e1693f1734293df5b8c8d806fa53f
joseryandm <joae.ryan.123@gmail.com>
2022-04-19 14:38:58 -0300

    tornando Currency() um atributo privado em dollar e franc

--- src/Dollar.java
+++ src/Dollar.java
@@ -1,11 +1,13 @@
 public class Dollar extends Money {
+  private String currency;
   Dollar(int amount) {
     this.amount= amount;
+    currency = "USD";
   }
   Money times(int multiplier) {
     return new Dollar(amount * multiplier);
   }
   String currency() {
-    return "USD";
+    return currency;
   }  
 }
--- src/Franc.java
+++ src/Franc.java
@@ -1,11 +1,13 @@
 public class Franc extends Money{				
+  private String currency;
   Franc(int amount) {      
      this.amount= amount;
+     currency = "CHF";
   }					
   Money times(int multiplier)  {
     return new Franc(amount * multiplier);
   }	
   String currency() {
-    return "CHF";
-  }
+    return currency;
+ }
 }
