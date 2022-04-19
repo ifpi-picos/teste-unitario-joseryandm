9fa068c2209e203b355b32b9afbc1f4638ba7eb6
joseryandm <joae.ryan.123@gmail.com>
2022-04-19 15:35:40 -0300

    transformando money em uma classe concreta

--- src/Franc.java
+++ src/Franc.java
@@ -1,8 +1,5 @@
 public class Franc extends Money{				
   Franc(int amount, String currency) {
     super(amount, currency);
-  }					
-  Money times(int multiplier)  {
-    return Money.franc(amount * multiplier);
   }
 }
--- src/MoneyTest.java
+++ src/MoneyTest.java
@@ -4,14 +4,15 @@ import org.junit.Test;
 public class MoneyTest {
   @Test
   public void testEquality() {
-    assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+    assertTrue(Money.dollar(5).equals(Money.dollar(5))); 
     assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-    assertTrue(Money.franc(5).equals(Money.franc(5)));
-    assertFalse(Money.franc(5).equals(Money.franc(6)));
     assertFalse(Money.franc(5).equals(Money.dollar(5)));
   }
   public void testCurrency() {
     assertEquals("USD", Money.dollar(1).currency());
     assertEquals("CHF", Money.franc(1).currency());
+  }
+  public void testDifferentClassEquality() {
+    assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
   }
 }
