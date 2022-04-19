c89dd7dd626585da03bcb247b9e07c253d86c8c5
joseryandm <joae.ryan.123@gmail.com>
2022-04-19 15:43:31 -0300

    Remover as classes Dollar e Franc, alterar referências na classe Money

--- src/MoneyTest.java
+++ src/MoneyTest.java
@@ -2,17 +2,20 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class MoneyTest {
-  @Test
-  public void testEquality() {
-    assertTrue(Money.dollar(5).equals(Money.dollar(5))); 
-    assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-    assertFalse(Money.franc(5).equals(Money.dollar(5)));
-  }
-  public void testCurrency() {
-    assertEquals("USD", Money.dollar(1).currency());
-    assertEquals("CHF", Money.franc(1).currency());
-  }
-  public void testDifferentClassEquality() {
-    assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
-  }
+    @Test
+    public void testEquality() {
+        assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+        assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+        assertFalse(Money.franc(5).equals(Money.dollar(6)));
+    }
+    public void testMultiplication() {
+        Money five = Money.dollar(5);
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
+     }
+    @Test
+    public void testCurrency() {
+        assertEquals("USD", Money.dollar(1).currency());
+        assertEquals("CHF", Money.franc(1).currency());
+     }
 }
