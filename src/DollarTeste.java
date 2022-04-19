2097fdf2de2f0d31c14dc857dbfc5f2b82629197
joseryandm <joae.ryan.123@gmail.com>
2022-04-19 14:24:45 -0300

    adicionando as classes dollar e franc em money pra retornar uma instancia

--- src/DollarTeste.java
+++ src/DollarTeste.java
@@ -1,17 +1,16 @@
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class DollarTest {
   @Test
   public void testMultiplication(){
-    Dollar five = new Dollar(5);
-    assertEquals(new Dollar(10), five.times(2));
-    assertEquals(new Dollar(15), five.times(3));
+    Money five = Money.dollar(5);
+    assertEquals(Money.dollar(10), five.times(2));
+    assertEquals(Money.dollar(15), five.times(3));
   }
-
   @Test
   public void testEquality() {
     assertTrue(new Dollar(5).equals(new Dollar(5)));
     assertFalse(new Dollar(5).equals(new Dollar(6)));
   }
+}
